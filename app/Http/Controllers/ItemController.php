d9ae05699cb93b817632099e954ccc550dca692b
Pablo <pgonzalezs1999@gmail.com>
2024-12-08 16:22:18 +0100

    Update ItemController.php

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -105,8 +105,11 @@ class ItemController extends Controller
         }
         $user = Auth::user();
         $businesses = Utils::getBusinessesFromDistance(
-            $request -> latitude, $request -> longitude, 0.5
+            $request -> latitude,
+            $request -> longitude,
+            0.5,
         );
+        $businesses = Business::all();
         foreach($businesses as $business) {
             $distance = Utils::get2dDistance($request -> longitude, $request -> latitude, $business -> longitude, $business -> latitude);
             $business -> distance = $distance;
